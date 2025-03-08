6902dd13c8cb9851865730802f3e1de18b2b972d
D.B <derek.baugh1775@gmail.com>
2025-03-09 04:41:27 +0700

    server harden test

--- server-harden.sh
+++ server-harden.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 sudo ansible-pull -U "https://github.com/BCL-FOSS/Linux-Utils.git" -i localhost, -c local server-harden.yml \
-                --extra-vars "instance_id=${INSTANCE_ID} region=${REGION} public_ip=${PUBLIC_IP} hostname=${HOSTNAME} os=${OS}"
+                --extra-vars "sudouser=$1 username=$2 upwd=$3 rpwd=$4 timezone=$5 ctrl_ip=$6 ctrl_url=$7 ctrl_url_short=$8"
 
 

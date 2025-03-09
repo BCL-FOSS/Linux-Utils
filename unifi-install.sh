5fa8535f57f1495ee07ed927cd6dfcdd008a2622
D.B <derek.baugh1775@gmail.com>
2025-03-10 02:15:55 +0700

    unifi install workflow config

---
+++ unifi-install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Pull and run unifi network controller installation playbook for Production Ubuntu Server
+
+sudo ansible-pull -U "https://github.com/BCL-FOSS/Linux-Utils.git" -i localhost, -c local unifi-install.yml \
+                --extra-vars "sudouser=$1"
+
+

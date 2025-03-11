fb22f2946268e960980ad2e18e0cc9ca190dca84
D.B <derek.baugh1775@gmail.com>
2025-03-12 02:24:51 +0700

    server hardening update

--- server-harden.sh
+++ server-harden.sh
@@ -2,6 +2,6 @@
 # Pull and run server hardening playbook for Production Ubuntu Server
 
 sudo ansible-pull -U "https://github.com/BCL-FOSS/Linux-Utils.git" -i localhost, -c local server-harden.yml \
-                --extra-vars "sudouser=$SUDOUSER ctrl_ip=$IP ssh_pub_key=$PUBKEY user_ssh_pub_key=$USERPUBKEY user=$USERNAME user_passwd=$USERPWD root_passwd=$ROOTPWD"
+                --extra-vars "sudouser=$SUDOUSER ctrl_ip=$IP"
 
 

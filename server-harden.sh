6489106b69cb3dea90b0cc359f4e9f8cedbb736e
D.B <derek.baugh1775@gmail.com>
2025-03-14 18:28:18 +0700

    Update server-harden.sh

--- server-harden.sh
+++ server-harden.sh
@@ -1,34 +1,13 @@
 #!/bin/sh
 # Pull and run server hardening playbook for Production Ubuntu Server
 
-install_pipx() {
-
-    if ! command -v pipx &> /dev/null; then
-        echo "pipx not found. Installing..."
-        sudo apt install -y pipx
-        pipx ensurepath
-    else
-        echo "pipx is already installed."
-        pipx ensurepath
-    fi
-}
-
-install_ansible() {
-
-    if ! command -v ansible &> /dev/null; then
-        echo "Installing Ansible..."
-        pipx install --include-deps ansible
-        
-        echo "Ansible installation complete."
-    else
-        echo "Ansible is already installed."
-    fi
-}
-
 sudo apt update
 
-install_pipx
-install_ansible
+sudo apt install -y pipx
+pipx ensurepath
+
+pipx install --include-deps -y ansible
+pipx ensurepath
 
 #sudo ansible-pull -U "https://github.com/BCL-FOSS/Linux-Utils.git" -i localhost, -c local server-harden.yml \
 #                --extra-vars "sudouser=$1 ctrl_ip=$2"

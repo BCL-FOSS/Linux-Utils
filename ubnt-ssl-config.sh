6aadc6239fe62ce478a6b6aa69518d0d389f4679
D.B <derek.baugh1775@gmail.com>
2025-03-17 01:50:06 +0700

    ubnt ssl update

--- ubnt-ssl-config.sh
+++ ubnt-ssl-config.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-sudo apt install snapd
-sudo snap install core; sudo snap refresh core
-sudo apt install certbot python3-certbot-apache
-sudo ln -s /snap/bin/certbot /usr/bin/certbot
+sudo apt update && sudo apt install certbot python3-certbot-apache -y
 sudo certbot --apache -d $1
-sudo certbot -d $1
-sudo certbot certonly -d $1
 sudo wget https://raw.githubusercontent.com/BCL-FOSS/Linux-Utils/refs/heads/main/unifi_ssl_deb_import.sh -O /usr/local/bin/unifi_ssl_deb_import.sh
 sudo chmod +x /usr/local/bin/unifi_ssl_deb_import.sh
 # sudo nano -w /usr/local/bin/unifi_ssl_deb_import.sh

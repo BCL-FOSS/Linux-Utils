770d951ab78a355c755479937dc128f6b3507c50
D.B <derek.baugh1775@gmail.com>
2025-03-16 20:12:01 +0700

    Update server-harden.sh

--- server-harden.sh
+++ server-harden.sh
@@ -6,15 +6,11 @@ sudo apt update
 sudo apt install -y pipx
 pipx ensurepath
 
-# Install Ansible with dependencies
+pipx list
 
 pipx install --include-deps ansible
-
-# Verify installation
-pipx list
-# ansible --version
+pipx ensurepath
 
 #sudo ansible-pull -U "https://github.com/BCL-FOSS/Linux-Utils.git" -i localhost, -c local server-harden.yml \
 #                --extra-vars "sudouser=$1 ctrl_ip=$2"
-
 

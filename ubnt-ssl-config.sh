35637b8733bd04e5df466ff17857b244bfa33cd0
D.B <derek.baugh1775@gmail.com>
2025-03-17 20:14:11 +0700

    Update ubnt-ssl-config.sh

--- ubnt-ssl-config.sh
+++ ubnt-ssl-config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sudo apt update && sudo apt install certbot python3-certbot-apache -y
-sudo certbot --apache --email $1 --no-eff-email -d $2
-sudo wget https://raw.githubusercontent.com/BCL-FOSS/Linux-Utils/refs/heads/main/unifi_ssl_deb_import.sh -O /usr/local/bin/unifi_ssl_deb_import.sh
-sudo chmod +x /usr/local/bin/unifi_ssl_deb_import.sh
+sudo certbot --apache --email $1 --no-eff-email --agree-tos -n -d $2
+sudo wget https://raw.githubusercontent.com/BCL-FOSS/Linux-Utils/refs/heads/main/unifi_ssl_deb_import.sh -O /run/init_scripts/unifi_ssl_deb_import.sh
+# sudo chmod +x /usr/local/bin/unifi_ssl_deb_import.sh
 # sudo nano -w /usr/local/bin/unifi_ssl_deb_import.sh
-sudo /usr/local/bin/unifi_ssl_deb_import.sh $2
+sudo /run/init_scripts/unifi_ssl_deb_import.sh $2
